15037281c0d1f7b60c155e8c1ba963cbfc885b51
pedro de la cruz <pedrom.g712@hotmail.com>
2021-03-02 14:22:52 -0400

    Add project files.

---
+++ PruebaIngreso/Controllers/PokemonController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace PruebaIngreso.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+
+        [HttpGet("{name}")]
+        public  IActionResult getPokemon(string name) {
+
+            string url = "https://pokeapi.co/api/v2/pokemon/" + name;
+
+            using (WebClient json = new WebClient())
+            {
+                var datos =  json.DownloadString(url);
+                return Ok(datos);
+            }
+        }
+    }
+}
